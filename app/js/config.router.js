32cb4a47b86d0f8bb7fb12f1cb4be2c6ebe1cd27
yuliantofrandi <yuliantofrandi@laptop1>
2015-06-25 09:40:12 +0700

    Merge origin/master
    Conflicts:
    	index.html

---
+++ app/js/config.router.js
@@ -0,0 +1,163 @@
+'use strict';
+
+/**
+ * Config for the router
+ */
+angular.module('app')
+        .run(
+                ['$rootScope', '$state', '$stateParams',
+                    function ($rootScope, $state, $stateParams) {
+                        $rootScope.$state = $state;
+                        $rootScope.$stateParams = $stateParams;
+                    }
+                ]
+                )
+        .config(
+                ['$stateProvider', '$urlRouterProvider',
+                    function ($stateProvider, $urlRouterProvider) {
+
+                        $urlRouterProvider
+                                .otherwise('/app/dashboard');
+                        $stateProvider
+                                .state('app', {
+                                    abstract: true,
+                                    url: '/app',
+                                    templateUrl: 'tpl/app.html'
+                                })
+                                .state('app.dashboard', {
+                                    url: '/dashboard',
+                                    templateUrl: 'tpl/dashboard.html',
+                                    resolve: {
+                                        deps: ['$ocLazyLoad',
+                                            function ($ocLazyLoad) {
+
+                                            }]
+                                    }
+                                })
+                                .state('app.pegawai', {
+                                    url: '/pegawai',
+                                    templateUrl: 'tpl/m_pegawai/index.html',
+                                    resolve: {
+                                        deps: ['$ocLazyLoad',
+                                            function ($ocLazyLoad) {
+                                                return $ocLazyLoad.load('js/controllers/pegawai.js');
+                                            }]
+                                    }
+                                })
+                                // others
+                                .state('access', {
+                                    url: '/access',
+                                    template: '<div ui-view class="fade-in-right-big smooth"></div>'
+                                })
+                                .state('access.signin', {
+                                    url: '/signin',
+                                    templateUrl: 'tpl/page_signin.html',
+                                    resolve: {
+                                        deps: ['uiLoad',
+                                            function (uiLoad) {
+                                                return uiLoad.load(['js/controllers/signin.js']);
+                                            }]
+                                    }
+                                })
+                                .state('access.404', {
+                                    url: '/404',
+                                    templateUrl: 'tpl/page_404.html'
+                                })
+                                //master
+                                .state('master', {
+                                    url: '/master',
+                                    templateUrl: 'tpl/app.html'
+                                })
+                                .state('master.roles', {
+                                    url: '/roles',
+                                    templateUrl: 'tpl/m_roles/index.html',
+                                    resolve: {
+                                        deps: ['$ocLazyLoad',
+                                            function ($ocLazyLoad) {
+                                                return $ocLazyLoad.load('js/controllers/roles.js');
+                                            }]
+                                    }
+                                })
+                                .state('master.satuan', {
+                                    url: '/satuan',
+                                    templateUrl: 'tpl/m_satuan/index.html',
+                                    resolve: {
+                                        deps: ['$ocLazyLoad',
+                                            function ($ocLazyLoad) {
+                                                return $ocLazyLoad.load('js/controllers/satuan.js');
+                                            }]
+                                    }
+                                })
+                                .state('master.pengguna', {
+                                    url: '/pengguna',
+                                    templateUrl: 'tpl/m_user/index.html',
+                                    resolve: {
+                                        deps: ['$ocLazyLoad',
+                                            function($ocLazyLoad) {
+                                                return $ocLazyLoad.load('js/controllers/pengguna.js');
+                                            }]
+                                    }
+                                })
+                                .state('master.klinik', {
+                                    url: '/klinik',
+                                    templateUrl: 'tpl/m_klinik/index.html',
+                                    resolve: {
+                                        deps: ['$ocLazyLoad',
+                                            function($ocLazyLoad) {
+                                                return $ocLazyLoad.load('js/controllers/klinik.js');
+                                            }]
+                                    }
+                                })
+                                .state('master.customer', {
+                                    url: '/customer',
+                                    templateUrl: 'tpl/m_customer/index.html',
+                                    resolve: {
+                                        deps: ['$ocLazyLoad',
+                                            function($ocLazyLoad) {
+                                                return $ocLazyLoad.load('js/controllers/customer.js');
+                                            }]
+                                    }
+                                })
+                                .state('master.supplier', {
+                                    url: '/supplier',
+                                    templateUrl: 'tpl/m_supplier/index.html',
+                                    resolve: {
+                                        deps: ['$ocLazyLoad',
+                                            function($ocLazyLoad) {
+                                                return $ocLazyLoad.load('js/controllers/supplier.js');
+                                            }]
+                                    }
+                                })
+                                .state('master.karyawan', {
+                                    url: '/karyawan',
+                                    templateUrl: 'tpl/m_karyawan/index.html',
+                                    resolve: {
+                                        deps: ['$ocLazyLoad',
+                                            function($ocLazyLoad) {
+                                                return $ocLazyLoad.load('js/controllers/karyawan.js');
+                                            }]
+                                    }
+                                })
+                                .state('master.kategori', {
+                                    url: '/kategori',
+                                    templateUrl: 'tpl/m_kategori/index.html',
+                                    resolve: {
+                                        deps: ['$ocLazyLoad',
+                                            function($ocLazyLoad) {
+                                                return $ocLazyLoad.load('js/controllers/kategori.js');
+                                            }]
+                                    }
+                                })
+                                .state('master.barang', {
+                                    url: '/barang',
+                                    templateUrl: 'tpl/m_barang/index.html',
+                                    resolve: {
+                                        deps: ['$ocLazyLoad',
+                                            function($ocLazyLoad) {
+                                                return $ocLazyLoad.load('js/controllers/barang.js');
+                                            }]
+                                    }
+                                })
+                    }
+                ]
+                );
---
+++ app/js/controllers/roles.js
@@ -0,0 +1,107 @@
+app.controller('rolesCtrl', function ($scope, Data, toaster) {
+    //init data
+    var tableStateRef;
+    $scope.displayed = [];
+    $scope.is_edit = false;
+    $scope.is_view = false;
+    $scope.akses = {master:{},transaksi:{}};
+    console.log($scope.aa);
+    $scope.callServer = function callServer(tableState) {
+        tableStateRef = tableState;
+        $scope.isLoading = true;
+        var offset = tableState.pagination.start || 0;
+        var limit = tableState.pagination.number || 10;
+        var param = {offset: offset, limit: limit};
+
+        if (tableState.sort.predicate) {
+            param['sort'] = tableState.sort.predicate;
+            param['order'] = tableState.sort.reverse;
+        }
+        if (tableState.search.predicateObject) {
+            param['filter'] = tableState.search.predicateObject;
+        }
+
+        Data.get('roles', param).then(function (data) {
+            $scope.displayed = data.data;
+            tableState.pagination.numberOfPages = Math.round(data.totalItems / limit);
+        });
+
+        $scope.isLoading = false;
+    };
+
+    $scope.create = function (form) {
+        $scope.is_edit = true;
+        $scope.is_view = false;
+        $scope.formtitle = "Form Tambah Data";
+        $scope.form = {};
+    };
+    $scope.update = function (form) {
+        $scope.is_edit = true;
+        $scope.is_view = false;
+        $scope.formtitle = "Edit Data : " + form.nama;
+        $scope.form = form;
+    };
+    $scope.view = function (form) {
+        $scope.is_edit = true;
+        $scope.is_view = true;
+        $scope.formtitle = "Lihat Data : " + form.nama;
+        $scope.form = form;
+    };
+    $scope.save = function (form) {
+        var url = (form.id > 0) ? 'roles/update/' + form.id : 'roles/create';
+        Data.post(url, form).then(function (result) {
+            if (result.status == 0) {
+                toaster.pop('error', "Terjadi Kesalahan", result.errors);
+            } else {
+                $scope.is_edit = false;
+                $scope.callServer(tableStateRef); //reload grid ulang
+                toaster.pop('success', "Berhasil", "Data berhasil tersimpan");
+            }
+        });
+    };
+    $scope.cancel = function () {
+        $scope.is_edit = false;
+        $scope.is_view = false;
+    };
+
+    $scope.trash = function (row) {
+        if (confirm("Apa anda yakin akan MENGHAPUS item ini ?")) {
+            row.is_deleted = 1;
+            Data.post('roles/update/' + row.id, row).then(function (result) {
+                $scope.displayed.splice($scope.displayed.indexOf(row), 1);
+            });
+        }
+    };
+    $scope.restore = function (row) {
+        if (confirm("Apa anda yakin akan MERESTORE item ini ?")) {
+            row.is_deleted = 0;
+            Data.post('roles/update/' + row.id, row).then(function (result) {
+                $scope.displayed.splice($scope.displayed.indexOf(row), 1);
+            });
+        }
+    };
+    $scope.delete = function (row) {
+        if (confirm("Apa anda yakin akan MENGHAPUS PERMANENT item ini ?")) {
+            Data.delete('roles/delete/' + row.id).then(function (result) {
+                $scope.displayed.splice($scope.displayed.indexOf(row), 1);
+            });
+        }
+    };
+    
+    $scope.checkMaster = function () {
+//        console.log($scope.akses);
+//        if ($scope.selectMaster) {
+//            $scope.selectMaster = true;
+//        } else {
+//            $scope.selectMaster = false;
+//        }
+console.log($scope.akses);
+        angular.forEach($scope.akses, function (item) {
+            
+////            item.Selected = $scope.selectedAll;
+        });
+
+    };
+
+
+})
